7d920463237b2cfa93b7061d637674d84b14575d
Marek Szkudelski <marek.szkudelski@allegro.pl>
2023-12-18 14:28:03 +0100

    day 18 wip

---
+++ day18.js
@@ -0,0 +1,23 @@
+const example = `R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)`
+
+function getCubicMeters(data) {
+    return data.split('\n').map(line => {
+        const [_, direction, distance] = line.match(/([RLDU]) (\d*)/)
+        return {distance, direction,}
+    })
+}
+
+console.log(getCubicMeters(example))
