9e4e35bf21346a2c19ceda090725de239ded3950
Marek Szkudelski <marek.szkudelski@allegro.pl>
2023-12-18 18:41:25 +0100

    day 17 wip

--- day17.js
+++ day17.js
@@ -15,6 +15,7 @@ const example = `2413432311323
 function getHeatLoss(data) {
     const matrix = data.split('\n').map(row => row.split('').map(Number));
 
+    let prev = {x: -1, y: -1};
     let current = {x: 0, y: 0};
     let heatLoss = 0;
 
@@ -22,21 +23,23 @@ function getHeatLoss(data) {
         // let value = Number(matrix[current.x][current.y]);
         const neighbors = [{x: current.x + 1, y: current.y}, {x: current.x - 1, y: current.y},
             {x: current.x, y: current.y - 1}, {x: current.x, y: current.y + 1}]
-
-        let minLoss = neighbors[0];
-        for (let i = 1; i < neighbors.length; i++) {
+        console.log({current, prev})
+        let minLoss = null;
+        for (let i = 0; i < neighbors.length; i++) {
             const {x, y} = neighbors[i];
             console.log('prev', {
                 currentNeigbor: neighbors[i],
                 currentMin: minLoss,
                 i,
             })
-            if (matrix[minLoss.y] === undefined || matrix[minLoss.y][minLoss.x] === undefined) {
+            if (matrix[minLoss?.y] === undefined || matrix[minLoss?.y][minLoss.x] === undefined) {
                 minLoss = neighbors[i]
             }
-            if (matrix[y] === undefined || matrix[y][x] === undefined) {
-                i++;
+            if (matrix[y] === undefined || matrix[y][x] === undefined || (y === prev[y] && x === prev[x])) {
+                console.log('continue')
+                continue
             }
+            // console.log({x, y, prev})
             // console.log('mid', {
             //     currentNeigbor: neighbors[i],
             //     currentMin: minLoss,
@@ -44,7 +47,7 @@ function getHeatLoss(data) {
             //     nValue: matrix[y]?.[x],
             //     lossValue: matrix[minLoss.y][minLoss.x]
             // })
-            if (matrix[y]?.[x] < matrix[minLoss.y][minLoss.x]) {
+            if (minLoss === null || matrix[y]?.[x] < matrix[minLoss.y][minLoss.x]) {
                 minLoss = neighbors[i];
             }
             // console.log('after', {currentNeigbor: neighbors[i], currentMin: minLoss})
@@ -53,6 +56,7 @@ function getHeatLoss(data) {
 
         console.log(minLoss);
         heatLoss += Number(matrix[minLoss.y]?.[minLoss.x]);
+        prev = current;
         current = minLoss
         // console.log(heatLoss)
     }
