be80072eb762b8df08d17be240138e2bd17fe14e
Marek Szkudelski <marek.szkudelski@allegro.pl>
2023-12-18 18:03:33 +0100

    day 17 wip

---
+++ day17.js
@@ -0,0 +1,62 @@
+const example = `2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533`
+
+function getHeatLoss(data) {
+    const matrix = data.split('\n').map(row => row.split('').map(Number));
+
+    let current = {x: 0, y: 0};
+    let heatLoss = 0;
+
+    while (current.x < matrix.length && current.y < matrix[0].length && heatLoss < 102) {
+        // let value = Number(matrix[current.x][current.y]);
+        const neighbors = [{x: current.x + 1, y: current.y}, {x: current.x - 1, y: current.y},
+            {x: current.x, y: current.y - 1}, {x: current.x, y: current.y + 1}]
+
+        let minLoss = neighbors[0];
+        for (let i = 1; i < neighbors.length; i++) {
+            const {x, y} = neighbors[i];
+            console.log('prev', {
+                currentNeigbor: neighbors[i],
+                currentMin: minLoss,
+                i,
+            })
+            if (matrix[minLoss.y] === undefined || matrix[minLoss.y][minLoss.x] === undefined) {
+                minLoss = neighbors[i]
+            }
+            if (matrix[y] === undefined || matrix[y][x] === undefined) {
+                i++;
+            }
+            // console.log('mid', {
+            //     currentNeigbor: neighbors[i],
+            //     currentMin: minLoss,
+            //     i,
+            //     nValue: matrix[y]?.[x],
+            //     lossValue: matrix[minLoss.y][minLoss.x]
+            // })
+            if (matrix[y]?.[x] < matrix[minLoss.y][minLoss.x]) {
+                minLoss = neighbors[i];
+            }
+            // console.log('after', {currentNeigbor: neighbors[i], currentMin: minLoss})
+
+        }
+
+        console.log(minLoss);
+        heatLoss += Number(matrix[minLoss.y]?.[minLoss.x]);
+        current = minLoss
+        // console.log(heatLoss)
+    }
+    return heatLoss
+}
+
+console.log(getHeatLoss(example));
